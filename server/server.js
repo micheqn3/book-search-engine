8978f48e43b82f4970a5b281b614f20bf8ae2053
Michelle Nguyen <michellenguyen11239@aol.com>
2021-08-09 11:21:48 -0700

    Implemented apollo server in server.js and added schemas folder

--- server/server.js
+++ server/server.js
@@ -1,21 +1,43 @@
+// Import dependencies and db connection
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
 
+// Set up express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Implement the apollo server with the GraphQL schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// Apply it to the Express server as middleware
+server.applyMiddleware({ app });
+
+// Sets up data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// If in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Set up a wildcard route on our server that will serve the front end whenever a request for a non-API route is received
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
+// Start the express server and graphql playground
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
