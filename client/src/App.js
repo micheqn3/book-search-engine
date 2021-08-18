d2630234d8b4c018bb40e20599977c673f74ed6f
Michelle Nguyen <michellenguyen11239@aol.com>
2021-08-17 17:05:23 -0700

    Updated switch routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -36,15 +36,13 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-            <>
-              <Navbar />
-              <Switch>
-                <Route exact path='/' component={SearchBooks} />
-                <Route exact path='/saved' component={SavedBooks} />
-                <Route component={ErrorPage} />
-              </Switch>
-            </>
-          </Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path='/' component={SearchBooks} />
+          <Route path='/saved' component={SavedBooks} />
+          <Route path="*" component={ErrorPage} />
+        </Switch>
+      </Router>
     </ApolloProvider>
   );
 }
